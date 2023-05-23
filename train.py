fed99578b9afa2d4897333b897c7e4c609da2ab9
petrutiuvlad <37187104+petrutiuvlad@users.noreply.github.com>
2023-05-24 01:07:15 +0300

    10 epoch

--- train.py
+++ train.py
@@ -5,5 +5,5 @@ model = YOLO("yolov8s.yaml")  # build a new model from scratch
 model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="dataset.yaml", epochs=10, patience=5, batch=128)  # train the model
+model.train(data="dataset.yaml", epochs=10, patience=10, batch=128)  # train the model
 # model.save("/valohai/outputs/models/trained.pt")  # save trained model to trained.pt
