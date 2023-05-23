8389e42b77e6dcd5cd7b1fee840c0ec0b64b2112
petrutiuvlad <37187104+petrutiuvlad@users.noreply.github.com>
2023-05-23 16:43:25 +0300

    valohai

---
+++ train.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8s.yaml")  # build a new model from scratch
+model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+model.train(data="dataset.yaml", epochs=1)  # train the model
+model.save("/valohai/outputs/models/trained.pt")  # save trained model to trained.pt
