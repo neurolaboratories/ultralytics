b1744c0f00c60b3e0aec6da68e6689b7d2d1a5d8
petrutiuvlad <37187104+petrutiuvlad@users.noreply.github.com>
2023-06-13 14:39:08 +0300

    20 epochs

--- train.py
+++ train.py
@@ -4,5 +4,5 @@ from ultralytics import YOLO
 model = YOLO("yolov8s-seg.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="dataset.yaml", epochs=1, patience=5, batch=128)  # train the model
+model.train(data="dataset.yaml", epochs=20, patience=5, batch=128)  # train the model
 # model.save("/valohai/outputs/models/trained.pt")  # save trained model to trained.pt
