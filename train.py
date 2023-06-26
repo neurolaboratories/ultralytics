ed3beede0bc3c839355bb17c412c75130c23f0dc
petrutiuvlad <37187104+petrutiuvlad@users.noreply.github.com>
2023-06-26 13:46:25 +0300

    train detect

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8s-seg.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
 
-# Use the model
-model.train(data="dataset.yaml", epochs=20, patience=5, batch=128)  # train the model
-# model.save("/valohai/outputs/models/trained.pt")  # save trained model to trained.pt
+# # Use the model
+model.train(data="dataset.yaml", epochs=10, patience=5, batch=128)  # train the model
+# # model.save("/valohai/outputs/models/trained.pt")  # save trained model to trained.pt
